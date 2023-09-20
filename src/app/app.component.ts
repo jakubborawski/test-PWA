75eebecc594b4c6d4074ea8ce01798b341551adf
jakubborawski <jakub.borawski.praca@gmail.com>
2023-09-20 20:51:02 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor( private swUpdate: SwUpdate) {}
+
+  ngOnInit() {
+    // if (this.swUpdate.isEnabled) {
+    //   this.swUpdate.versionUpdates.subscribe(() => {
+    //     if(confirm("You're using an old version of the control panel. Want to update?")) {
+    //       window.location.reload();
+    //     }
+    //   });
+    // }
+  }
+
   title = 'test-application';
 }
